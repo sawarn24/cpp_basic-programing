7d428a5278ec40cfb2f352cd0cbd8e34a642c3bc
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-05-08 23:47:49 +0530

    Add files via upload

---
+++ sum_of_two_numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int num1,num2,sum;
+    cout<<"enter the numbers";
+    cin>>num1>>num2;
+    sum=num1+num2;
+    cout<<sum;
+    return 0;
+
+
+}
