9f72353e99c96fa52e1ceb1df2d251c0ef00d45c
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-05-14 21:06:11 +0530

    Add files via upload

---
+++ rectangular_pattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int l,w;
+cout<<"ENTER THE DIMENSION OF RECTANGLE"<<endl;
+cout<<"LENGTH: ";
+cin>>l;
+cout<<"WIDTH: ";
+cin>>w;
+for(int i=0;i<l;i++)
+{ for(int j=0;j<w;j++)
+  {
+    cout<<"* ";
+
+  }
+ cout<<endl;
+
+}
+return 0;
+
+
+}
