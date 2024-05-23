65e0123ea8982a7a30ad751aa0539f150ab09721
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-05-23 23:22:15 +0530

    Add files via upload

---
+++ armstrong_number.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main ()
+{
+    int n;
+    cout<<"ENTER A NUMBER : ";
+    cin>>n;
+    int sum=0;
+    int s=n;
+    while(s>0)
+    {
+        int lastdigit;
+        lastdigit=s%10;
+        sum+=pow(lastdigit,3);
+        s=s/10;
+    }
+    if(sum==n)
+        cout<<"ARMSTRONG NUMBER";
+    else
+        cout<<"NOT A ARMSTRONG NUMBER";
+    return 0;
+}
