3237de6699e948b3cf2c343b312e2d76a3a25771
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-07-03 20:36:32 +0530

    Add files via upload

---
+++ set_bit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,n;
+    cout<<"ENTER NUMBER : ";
+    cin>>num;
+    cout<<"ENTER POSITION : ";
+    cin>>n;
+
+       cout<<(num|(1<<n));
+    return 0;
+}
+
+
